29a64b0f9b56e079fe9698ec5006ad6b707b27af
Compulife <shoukry454@gmail.com>
2023-04-17 17:12:42 +0300

    add tow drawable, some changes on activity_view_cars.xml, model_data_profile.xml, model_rc_home.xml ViewCarsActivity.java. Done.

--- app/src/main/java/com/example/Screen_Home/ViewCarsActivity.java
+++ app/src/main/java/com/example/Screen_Home/ViewCarsActivity.java
@@ -2,6 +2,9 @@ package com.example.Screen_Home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -11,15 +14,29 @@ import com.example.projectcar.databinding.ActivityViewCarsBinding;
 public class ViewCarsActivity extends AppCompatActivity {
 
     ActivityViewCarsBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityViewCarsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.imgFavoriteCar.setOnClickListener(v -> {
-            binding.imgFavoriteCar.setImageDrawable(getDrawable(R.drawable.baseline_favorite_24));
-            Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
+        binding.checkboxFavoriteCar.setOnClickListener(v -> {
+            if (binding.checkboxFavoriteCar.isChecked()) {
+                binding.checkboxFavoriteCar.setForeground(getDrawable(R.drawable.favorite_checked_24));
+                Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show();
+            }
+            if (!binding.checkboxFavoriteCar.isChecked()) {
+                binding.checkboxFavoriteCar.setForeground(getDrawable(R.drawable.favorite_un_checked_24));
+                Toast.makeText(this, "Un Checked", Toast.LENGTH_SHORT).show();
+            }
         });
+
+        binding.fABCall.setOnClickListener(view ->{
+            Intent goToPhoneCalls = new Intent(Intent.ACTION_DIAL);
+            goToPhoneCalls.setData(Uri.parse("tel:0599791805"));
+            startActivity(goToPhoneCalls);
+        });
+
     }
 }
