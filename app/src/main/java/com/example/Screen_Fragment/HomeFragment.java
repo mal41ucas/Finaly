7b8751c51d0e22e24c293502e21d399de08cf7ac
Compulife <shoukry454@gmail.com>
2023-05-29 20:13:06 +0300

    edite for HomeScreenFragment

--- app/src/main/java/com/example/Screen_Fragment/HomeFragment.java
+++ app/src/main/java/com/example/Screen_Fragment/HomeFragment.java
@@ -106,7 +106,7 @@ public class HomeFragment extends Fragment {
 
     void getAllCars2() {
         requestQueue = Volley.newRequestQueue(getActivity());
-//        StringRequest stringRequest = new StringRequest(GET, "http://127.0.0.1/api/car", new Response.Listener<String>() {
+//        StringRequest stringRequest = new StringRequest(GET, "http://10.0.2.2:8000/api/car", new Response.Listener<String>() {
 //            @Override
 //            public void onResponse(String response) {
 //                try {
